afbad4fcbe70206253e8d42ebf99ff5ab971cdec
Marco Martinez <marco.mart1998@gmail.com>
2019-02-26 11:32:55 -0600

    Completed and returned the perimeter of Polygon

--- Polygon.java
+++ Polygon.java
@@ -33,5 +33,11 @@ public abstract class Polygon extends Shape
 	public double getPerimeter()
     {
     	// TODO: complete this...
+    	double perimeter = 0;
+    	for(int index = 0; index < sideLengths.size(); ++index)
+    	{
+    		perimeter = perimeter + sideLengths.get(index);
+    	}
+    	return perimeter;
     }
 }
