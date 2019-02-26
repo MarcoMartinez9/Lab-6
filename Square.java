e430f44e1415d648dc632f8becd6bd10a991daaa
Marco Martinez <marco.mart1998@gmail.com>
2019-02-26 12:27:38 -0600

    Completed Square class

--- Square.java
+++ Square.java
@@ -1,4 +1,15 @@
 
-public class Square {
+public class Square extends Rectangle
+{
+	
+	public Square(String id, double size) 
+	{
+		super(id, size, size);
+	}
+	
+	@Override
+	public String getShapeType() {
+		return "Square";
+	}
 
 }
