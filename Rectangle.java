32f3cc9ef240dc24791c38369be9ed98d4c54dcd
Marco Martinez <marco.mart1998@gmail.com>
2019-02-26 12:17:12 -0600

    Completed Rectangle class

--- Rectangle.java
+++ Rectangle.java
@@ -1,4 +1,25 @@
 
-public class Rectangle {
+public class Rectangle extends Polygon
+{
+
+	private double height, width;
+	
+	public Rectangle(String id, double height, double width) {
+		super(id);
+		this.height = height;
+		this.width = width;
+		sideLengths.add(height);
+		sideLengths.add(width);
+	}
+
+	@Override
+	public double getArea() {
+		return height * width;
+	}
+
+	@Override
+	public String getShapeType() {
+		return "Rectangle";
+	}
 
 }
