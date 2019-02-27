27a1a9b048f5cc5359b48db5b920873db143d17b
MarcoMartinez9 <47228260+MarcoMartinez9@users.noreply.github.com>
2019-02-26 22:43:59 -0600

    Added Todo.java

---
+++ Todo.java
@@ -0,0 +1,27 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-26-02
+OU ID_______________: 113424558
+OU EMAIL____________: marco.e.martinez-1@ou.edu
+
+Tasks:
+1. Create Square class
+2. Create Rectangle class
+3. Complete other shapes classes
+4. Completed Area and Perimeter Comparators
+5. Tested every method in testing classes
+
+Estimated Time (minutes):
+1. 20
+2. 20
+3. 20
+4. 30
+5. 40
+
+Actual Time (minutes):
+1. 20
+2. 20
+3. 25
+4. 15
+5. 50
+*/}
